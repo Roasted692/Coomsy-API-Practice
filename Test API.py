aee3b3af3319c100a60ab73264b057c11b062e6b
Zachary Louis <zlouis@thinksis.com>
2021-02-22 20:13:09 -0500

    First task down

--- Test API.py
+++ Test API.py
@@ -9,10 +9,6 @@ def basic_auth(username, password):
     else:
         return print("You're retarded")
 
-
-
-print(basic_auth("wayne", "Youre10PlyBud!"))
-
 #token = You'reInItToWinIt, from basic_auth(blah,blah)
 
 #gets list of alcohol available
@@ -21,9 +17,45 @@ def get_alcohol(token):
                             headers = {'Accept': 'application/json',
                                        'Token': token},
                             )
-    return print(response.json())
+    return response.json()
 
-get_alcohol(basic_auth("wayne", "Youre10PlyBud!"))
+def by_price(e):
+    return e
+
+#perform first activity
+def first_activity():
+    #grab list of alcohol and sort based on price from greatest to least
+    alcohol_number_prices = get_alcohol(basic_auth("wayne", "Youre10PlyBud!"))
+    alcohol_name_prices = []
+    alcohol_prices = []
+    most_expensive_alcohol = []
+    index = 0
+    for key in alcohol_number_prices:
+        alcohol_name_prices.append(alcohol_number_prices[key])
+        while index < len(alcohol_name_prices):
+            for other_key in alcohol_name_prices[index]:
+                alcohol_prices.append(alcohol_name_prices[index][other_key])
+            index += 1
+    alcohol_prices.sort(reverse=True)
+    most_expensive_price = alcohol_prices[0]
+    for x in alcohol_number_prices.values():
+        if most_expensive_price in x.values():
+            most_expensive_alcohol.append(list(x.keys()))
+    most_expensive_alcohol_name = most_expensive_alcohol[0]
+
+    # send fucking stupid answer
+    print(most_expensive_alcohol_name[0])
+    data = {'answer': "McDowell's No.1 Celebration"}
+    response = requests.post('http://127.0.0.1:5000/alcohol/activity/1',
+                             headers = {'Accept': 'application/json',
+                                        'Token': basic_auth("wayne", "Youre10PlyBud!")},
+                             json = data,
+                            )
+
+    return response.text
+
+
+print(first_activity())
 
 
 
