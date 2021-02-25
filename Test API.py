be5e55d2c243a76b44b7f1f751f57ef5af80b83f
Roasted692 <zach.louis@gmail.com>
2021-02-24 21:41:21 -0500

    latest updates, first 3 finished, started on 4th

--- Test API.py
+++ Test API.py
@@ -1,27 +1,39 @@
 import requests
 
-#auth endpoint http://127.0.0.1:5000/auth
+
+# auth endpoint http://127.0.0.1:5000/auth
 def basic_auth(username, password):
     response = requests.post('http://127.0.0.1:5000/auth',
-                            auth = (username, password))
+                             auth=(username, password))
     if response.status_code == 200:
         return response.headers['Token']
     else:
         return print("You're retarded")
 
-#token = You'reInItToWinIt, from basic_auth(blah,blah)
 
-#gets list of alcohol available
+# token = You'reInItToWinIt, from basic_auth(blah,blah)
+
+# gets list of alcohol available
 def get_alcohol(token):
     response = requests.get('http://127.0.0.1:5000/alcohol',
-                            headers = {'Accept': 'application/json',
-                                       'Token': token},
+                            headers={'Accept': 'application/json',
+                                     'Token': token},
                             )
     return response.json()
 
-#perform first activity
+
+def send_answers(data, activity):
+    response = requests.post(f'http://127.0.0.1:5000/alcohol/activity/{activity}',
+                             headers={'Accept': 'application/json',
+                                      'Token': basic_auth("wayne", "Youre10PlyBud!")},
+                             json=data,
+                             )
+    return response.text
+
+
+# perform first activity
 def first_activity():
-    #grab list of alcohol and sort based on price from greatest to least
+    # grab list of alcohol and sort based on price from greatest to least
     alcohol_number_prices = get_alcohol(basic_auth("wayne", "Youre10PlyBud!"))
     alcohol_name_prices = []
     alcohol_prices = []
@@ -39,20 +51,10 @@ def first_activity():
         if most_expensive_price in x.values():
             most_expensive_alcohol = list(x.keys())
 
-
     # send fucking stupid answer
     data = {'answer': f"{most_expensive_alcohol[0]}"}
-    response = requests.post('http://127.0.0.1:5000/alcohol/activity/1',
-                             headers = {'Accept': 'application/json',
-                                        'Token': basic_auth("wayne", "Youre10PlyBud!")},
-                             json = data,
-                            )
 
-    return response.text
-
-
-print(first_activity())
-
+    return print(send_answers(data, 1))
 
 
 def second_activity():
@@ -74,18 +76,10 @@ def second_activity():
         if most_expensive_price in x.values():
             most_expensive_alcohol = list(x.keys())
 
-
     # send fucking stupid answer
     data = {'answer': f"{most_expensive_alcohol[0]}"}
-    response = requests.post('http://127.0.0.1:5000/alcohol/activity/2',
-                             headers={'Accept': 'application/json',
-                                      'Token': basic_auth("wayne", "Youre10PlyBud!")},
-                             json=data,
-                             )
 
-    return response.text
-
-print(second_activity())
+    return print(send_answers(data, 2))
 
 def third_activity():
     alcohol_number_prices = get_alcohol(basic_auth("wayne", "Youre10PlyBud!"))
@@ -108,22 +102,10 @@ def third_activity():
                 top_10_most_expensive_alcohols.append(x)
 
     data = {'answer': top_10_most_expensive_alcohols}
-    response = requests.post('http://127.0.0.1:5000/alcohol/activity/3',
-                             headers={'Accept': 'application/json',
-                                      'Token': basic_auth("wayne", "Youre10PlyBud!")},
-                             json=data,
-                             )
 
-
-    return response.text
-
-
+    return print(send_answers(data, 3))
 
 def fourth_activity():
     alcohol_number_prices = get_alcohol(basic_auth("wayne", "Youre10PlyBud!"))
-
-
-
-print(third_activity())
 
 
